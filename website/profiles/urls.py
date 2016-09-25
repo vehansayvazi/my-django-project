091104f900e9018132b1d808b221d2cb9ec7c398
vehansayvazi <vehansayvazi5@gmail.com>
2016-09-25 02:14:33 -0700

    updates

--- website/profiles/urls.py
+++ website/profiles/urls.py
@@ -5,27 +5,10 @@ app_name ='profiles'
 
 urlpatterns = [
     # /profile/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index')
 
     # # /profile/
     # url(r'^program_detail/(?P<program_id>[0-9]+)/$', views.program_detail, name='program_detail'),
     #
-    # # /profile/
-    # url(r'^program_detail/$', views.program_detail, name='program_detail'),
-    #
-    # # /profile/
-    # url(r'^team_detail/(?P<team_id>[0-9]+)/$', views.team_detail, name='team_detail'),
-    #
-    # # /profile/
-    # url(r'^player_detail/(?P<player_id>[0-9]+)/$', views.player_detail, name='player_detail'),
-
-    # /profile/
-    #url(r'^team/$', views.detail, name='team_detail'),
-
-    # /profile/
-    #url(r'^player/$', views.detail, name='player_detail'),
-
-    # /profile/
-    #url(r'^coach/$', views.detail, name='coach_detail'),
 
 ]
--- website/profiles/views.py
+++ website/profiles/views.py
@@ -2,38 +2,29 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Program, Team, Player, Coach
 
-
-
 def index(request):
     all_program = Program.objects.all()
     all_team = Team.objects.all()
     all_player = Player.objects.all()
 
-    team8u = Player.objects.all().order_by('number').filter(team__name='8u Team') #need you use team__name becaue team is a ForeignKey to Team
+    team7u = Player.objects.all().order_by('number').filter(team__name='7u Team') #need to use team__name becaue team is a ForeignKey to Team
+    team8u = Player.objects.all().order_by('number').filter(team__name='8u Team') #need to use team__name becaue team is a ForeignKey to Team
     team9u = Player.objects.all().order_by('number').filter(team__name='9u Team')
     team10u = Player.objects.all().order_by('number').filter(team__name='10u Team')
     team11u = Player.objects.all().order_by('number').filter(team__name='11u Team')
+    team12u = Player.objects.all().order_by('number').filter(team__name='12u Team')
     team13u = Player.objects.all().order_by('number').filter(team__name='13u Team')
-    # rosters = {'team8u': team8u }
-               # 'team9u': team9u, 'team10u': team10u, 'team11u': team11u, 'team13u': team13u, }
+    team14u = Player.objects.all().order_by('number').filter(team__name='14u Team')
+    team15u = Player.objects.all().order_by('number').filter(team__name='15u Team')
+    team16u = Player.objects.all().order_by('number').filter(team__name='16u Team')
+    team17u = Player.objects.all().order_by('number').filter(team__name='17u Team')
 
     context = {'all_program': all_program, 'all_team': all_team, 'all_player': all_player,
-               'team8u': team8u, 'team9u': team9u, 'team10u': team10u, 'team11u': team11u, 'team13u': team13u}
+               'team7u': team7u, 'team8u': team8u, 'team9u': team9u, 'team10u': team10u, 'team11u': team11u,
+               'team12u': team12u, 'team13u': team13u, 'team14u': team14u,'team15u': team15u,
+               'team16u': team16u,'team17u': team17u,}
 
     return render(request, 'profiles/index.html', context)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # def program_detail(request, program_id):
